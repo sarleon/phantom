850c87586a24ff6e52af475967a7c4f58e6362c4
sarleon <sarleon56@hotmail.com>
2017-03-26 13:43:51 +0800

    init

---
+++ search_register.py
@@ -0,0 +1,23 @@
+import optparse
+
+def main():
+
+
+
+    '''
+    command line option parser
+    '''
+    option_parser=optparse.OptionParser()
+    option_parser.add_option('-e','--email',dest='email',action='store')
+    option_parser.add_option('-c','--cellphone',dest='cellphone',action='store')
+    option_parser.add_option('-t','--thread',dest='thread',action='store',default=10)
+    option_parser.add_option('-f','--file',dest='file',action='store')
+    email = option_parser.get_option('email')
+    cellphone = option_parser.get_option('cellphone')
+
+    if not email^cellphone:
+        option_parser.error('please use -c OR -e OR -f')
+
+
+if __name__ == '__main__':
+   main()
