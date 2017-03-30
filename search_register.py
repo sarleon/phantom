0caba55f4371b8b5058f142ee01ce74d5a4673bb
sarleon <sarleon56@hotmail.com>
2017-03-30 14:49:27 +0800

    add usage prompt

--- search_register.py
+++ search_register.py
@@ -2,32 +2,36 @@ from core.spider import Spider
 from core.taskTable import Tasktable
 import optparse
 
+
 def main():
-
-
-
     '''
     command line option parser
     '''
-    option_parser=optparse.OptionParser()
-    option_parser.add_option('-e','--email',dest='email',action='store')
-    option_parser.add_option('-c','--cellphone',dest='cellphone',action='store')
-    option_parser.add_option('-t','--thread',dest='thread',action='store',default=10)
 
+    option_parser = optparse.OptionParser()
+
+    option_parser.usage = \
+        """
+        search_register.py -e [email] [options]
+        search_register.py -c [cellphone] [options]"""
+
+    option_parser.add_option('-e', '--email', dest='email', action='store')
+    option_parser.add_option('-c', '--cellphone', dest='cellphone', action='store')
+    option_parser.add_option('-t', '--thread', dest='thread', action='store', default=10)
 
     email = option_parser.get_option('email')
     cellphone = option_parser.get_option('cellphone')
 
     if email:
-        task_list=Tasktable('email')
+        task_list = Tasktable('email')
     else:
         if cellphone:
-            task_list=Tasktable('cellphone')
+            task_list = Tasktable('cellphone')
 
         else:
-            raise KeyError
+            option_parser.error("must specify email OR cellphone")
 
 
 
 if __name__ == '__main__':
-   main()
+    main()
